1998f6c77b79c03415cbd0cc97a6cf25a3c0ec8d
nalingg <121306197+nalingg@users.noreply.github.com>
2025-11-09 19:38:43 +0900

    251109 07:38PM

--- dbcreate.sql
+++ dbcreate.sql
@@ -2,7 +2,7 @@ CREATE TABLE Team (
     team_ID INT PRIMARY KEY,
     team_Name VARCHAR(50) NOT NULL UNIQUE,
     home_arena VARCHAR(50)
-);
+)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE Player_Position (
     position_ID INT PRIMARY KEY,
@@ -18,7 +18,7 @@ CREATE TABLE User (
 
 CREATE TABLE Game (
     game_ID INT PRIMARY KEY,
-    game_Date TIMESTAMP NOT NULL,
+    game_Date DATE NOT NULL,
     round_ID INT NOT NULL, 
     home_team_ID INT NOT NULL,
     away_team_ID INT NOT NULL,
@@ -28,12 +28,13 @@ CREATE TABLE Game (
 
 CREATE TABLE Player (
     player_ID INT PRIMARY KEY,
+    player_name VARCHAR(50) NOT NULL,
     current_team_ID INT NOT NULL,
     position_ID INT NOT NULL,
     salary DECIMAL(12, 0),
     FOREIGN KEY (current_team_ID) REFERENCES Team(team_ID),
     FOREIGN KEY (position_ID) REFERENCES Player_Position(position_ID)
-);
+)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE Att_Stats (
     player_ID INT NOT NULL,
@@ -63,7 +64,7 @@ CREATE TABLE L_Stats (
     set_mis INT DEFAULT 0,
     receive_att INT DEFAULT 0,
     receive_good INT DEFAULT 0,
-    receive_miss INT DEFAULT 0,
+    receive_fail INT DEFAULT 0,
     PRIMARY KEY (player_ID, game_ID),
     FOREIGN KEY (player_ID) REFERENCES Player(player_ID),
     FOREIGN KEY (game_ID) REFERENCES Game(game_ID)
@@ -78,9 +79,6 @@ CREATE TABLE S_Stats (
     set_att INT DEFAULT 0,
     set_suc INT DEFAULT 0,
     set_mis INT DEFAULT 0,
-    receive_att INT DEFAULT 0,
-    receive_good INT DEFAULT 0,
-    receive_miss INT DEFAULT 0,
     PRIMARY KEY (player_ID, game_ID),
     FOREIGN KEY (player_ID) REFERENCES Player(player_ID),
     FOREIGN KEY (game_ID) REFERENCES Game(game_ID)
@@ -96,4 +94,4 @@ CREATE TABLE Scouting_Report (
     CONSTRAINT fk_report_user FOREIGN KEY (user_ID) REFERENCES `User`(user_ID), 
     CONSTRAINT fk_report_team FOREIGN KEY (team_ID) REFERENCES Team(team_ID),
     CONSTRAINT fk_report_player FOREIGN KEY (player_ID) REFERENCES Player(player_ID)
-);
+)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
